1a4e60f63282d67004e91effd100edadfccb8d44
andre <andremindurimg@gmail.com>
2023-04-22 19:35:51 -0300

    Update bin_multiple.c

--- src/bin_multiple.c
+++ src/bin_multiple.c
@@ -3,11 +3,26 @@
 #include <libgen.h>
 #include "nth_prime.h"
 
+/**
+  This function writes the given number to a binary file.
+  @param num The number to be written to the file.
+  @param data A pointer to a FILE struct representing the binary file.
+*/
+
 void callback(int num, void *data)
 {
   FILE *file = (FILE *)data;
   fwrite(&num, sizeof(num), 1, file);
 }
+
+/**
+  This function generates the nth prime number and stores the first n prime numbers
+  in a binary file. The file name is "multiple.bin" and is saved in the same directory
+  where the program is run.
+  @param argc The number of command line arguments passed to the program.
+  @param argv An array of strings containing the command line arguments.
+  @return The exit status of the program.
+*/
 
 int main(int argc, char **argv)
 {
