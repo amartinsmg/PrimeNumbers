630f729f711b9d10fb00a9d189b00dce7078cf7a
andre <andremindurimg@gmail.com>
2023-04-22 13:20:00 -0300

    Update db_multiblock.c

--- src/db_multiblock.c
+++ src/db_multiblock.c
@@ -4,6 +4,14 @@
 #include <libpq-fe.h>
 #include "nth_prime.h"
 
+/**
+  This function receives a prime number and a pointer to a PostgreSQL connection
+  object. It creates a SQL query to insert the prime number into a table named
+  tb_multi_block. The query is executed using the connection object.
+  @param num the prime number to be inserted into the table
+  @param data a void pointer to a PostgreSQL connection object
+*/
+
 void callback(int num, void *data)
 {
   PGconn *conn = (PGconn *)data;
@@ -17,6 +25,13 @@ void callback(int num, void *data)
     exit(-1);
   }
 }
+
+/**
+  The function connects to the database using the credentials specified in the connection string and
+  creates a table called tb_multi_block, calculates the n first prime numbers using a function named
+  nthPrime, and stores the numbers into the table using the callback function.
+  @return The exit status of the program.
+*/
 
 int main()
 {
